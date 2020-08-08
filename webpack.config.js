3ed95deb00ce08091c0413fcf5cab8bb7589afcc
NeoTRAN001 <neo_tran@outlook.com>
2020-08-07 19:25:46 -0500

    Config file for webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpack = require('html-webpack-plugin');
+
+const htmlWebpack = new HtmlWebpack({
+    template: './assets/index.template.html',
+    filename: 'index.html'
+});
+
+module.exports = {
+    entry: './assets/javascript/entry.js',
+    output: {
+        publicPath: '/',
+        path: path.join(__dirname, '..'),
+        filename: 'dist/javascript/bundle.js'
+    },
+    plugins: [ htmlWebpack ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    }
+}
