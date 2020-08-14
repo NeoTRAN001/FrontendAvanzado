6c24b62bdea6dae37c7964c9070e2ff3507746f4
NeoTRAN001 <neo_tran@outlook.com>
2020-08-13 21:49:57 -0500

    Content Slider un Js Object

---
+++ assets/javascript/slider/elements.js
@@ -0,0 +1,28 @@
+const elements = [
+    {
+        title: 'Lorem Ipsum',
+        subtitle: 'Ipsum',
+        image: './public/images/c.jpg',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate'
+    },
+    {
+        title: 'Lorem 2',
+        subtitle: 'Ipsum 2',
+        image: './public/images/3.jpg',
+        text: '2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate'
+    },
+    {
+        title: 'Lorsam 3',
+        subtitle: 'No tengo idea',
+        image: './public/images/1.jpg',
+        text: '2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate'
+    },
+    {
+        title: 'Nop #4',
+        subtitle: 'No tengo idea',
+        image: './public/images/4.jpg',
+        text: '2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate'
+    }
+]
+
+export default elements;
