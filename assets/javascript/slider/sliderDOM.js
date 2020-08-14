ed5c724a6cea312f60442bfa7638d5e2572caf18
NeoTRAN001 <neo_tran@outlook.com>
2020-08-13 21:51:46 -0500

    UPdate styles

--- assets/javascript/slider/sliderDOM.js
+++ assets/javascript/slider/sliderDOM.js
@@ -1,4 +1,5 @@
 import Slider from './slider';
+import elements from './elements';
 
 let sliderText     = document.querySelector("#slider-text");
 let sliderTitle    = document.querySelector("#slider-title");
@@ -7,26 +8,7 @@ let sliderImage    = document.querySelector("#slider-image");
 let textContent    = document.querySelector("#slider-text-content");
 
 let slider = new Slider({
-    elements: [
-        {
-            title: 'Lorem Ipsum',
-            subtitle: 'Ipsum',
-            image: './public/images/c.jpg',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate'
-        },
-        {
-            title: 'Lorem 2',
-            subtitle: 'Ipsum 2',
-            image: './public/images/3.jpg',
-            text: '2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate'
-        },
-        {
-            title: 'Lorsam 3',
-            subtitle: 'No tengo idea',
-            image: './public/images/1.jpg',
-            text: '2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate'
-        }
-    ],
+    elements: elements,
     animationFunc: function (element) {
         textContent.classList.add("hide");
         sliderImage.classList.add("hide");
